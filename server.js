d6df80e731f2b113b34ffccd16e44b13611903fc
aureleoules <aurele@oules.com>
2017-01-15 19:03:09 +0100

    Fixed getUserFeed route

--- server.js
+++ server.js
@@ -161,43 +161,37 @@ apiRoutes.get('/getUserFeed', function(req, res)  { //TODO: its gonna be hard
                 _id: 0,
                 followed: 1
             }).toArray(function(err, data) {
-                if (data[0].hasOwnProperty('followed'))  {
-                    res.json({
-                        success: false,
-                        msg: 'Utilisateur ne suit personne.'
-                    });
-                } else {
-                    var followedList = data[0].followed;
-                    if (followedList !== undefined)  {
-                        followedList.push(pseudo);
-                        var query = {};
-                        query['auteur'] = {
-                            "$in": followedList
-                        };
-                        MongoClient.connect(config.database, function(err, db) {
-                            if (err) {
-                                return console.dir(err);
-                            }
-                            var collection = db.collection('cours');
-                            collection.find(query,   {
-                                _id: 1,
-                                classe: 1,
-                                chapitre: 1,
-                                matiere: 1,
-                                titre: 1,
-                                lectures: 1,
-                                auteur: 1,
-                                createdAt: 1
-                            }).limit(50).toArray(function(err, data) {
-                                feed = data;
-                                res.json({
-                                    success: true,
-                                    feed: feed
-                                })
-                            });
+                var followedList = data[0].followed;
+                if (followedList !== undefined)  {
+                    followedList.push(pseudo);
+                    var query = {};
+                    query['auteur'] = {
+                        "$in": followedList
+                    };
+                    MongoClient.connect(config.database, function(err, db) {
+                        if (err) {
+                            return console.dir(err);
+                        }
+                        var collection = db.collection('cours');
+                        collection.find(query,   {
+                            _id: 1,
+                            classe: 1,
+                            chapitre: 1,
+                            matiere: 1,
+                            titre: 1,
+                            lectures: 1,
+                            auteur: 1,
+                            createdAt: 1
+                        }).limit(50).toArray(function(err, data) {
+                            feed = data;
+                            res.json({
+                                success: true,
+                                feed: feed
+                            })
                         });
-                    }
+                    });
                 }
+
             });
         });
     }
